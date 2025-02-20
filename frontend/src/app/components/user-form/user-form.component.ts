b6f7b94a09e9d91e20277cb0c7acfb87c2e7ee65
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-20 14:42:30 +0100

    add education and workExperience field

--- frontend/src/app/components/user-form/user-form.component.ts
+++ frontend/src/app/components/user-form/user-form.component.ts
@@ -5,6 +5,7 @@ import {
   FormBuilder,
   FormGroup,
   Validators,
+  FormArray,
 } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -14,11 +15,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { Applicant, Employee, WorkExperience } from '../../models/user.model';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     RouterModule,
     MatTabsModule,
@@ -45,12 +49,31 @@ export class UserFormComponent implements OnInit {
     this.userForm = this.createForm();
   }
 
+  get education(): FormArray {
+    return this.userForm.get('education') as FormArray;
+  }
+
+  get workExperience(): FormArray {
+    return this.userForm.get('workExperience') as FormArray;
+  }
+
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       if (params['id']) {
         const user = this.userService.getUserById(params['id']);
+        console.log('user found: ', user);
         if (user) {
           this.userForm.patchValue(user);
+          if (user.type == 'applicant') {
+            this.setEducation((user as Applicant).education);
+            this.workExperience.clear();
+          }
+
+          if (user.type == 'employee') {
+            this.setWorkExperience((user as Employee).workExperience);
+            this.education.clear();
+          }
+
           this.mode =
             this.route.snapshot.url[2]?.path === 'view' ? 'view' : 'edit';
           if (this.mode === 'view') {
@@ -63,6 +86,7 @@ export class UserFormComponent implements OnInit {
 
   createForm(): FormGroup {
     return this.fb.group({
+      id: [''],
       documentId: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName1: ['', Validators.required],
@@ -86,12 +110,71 @@ export class UserFormComponent implements OnInit {
     if (this.userForm.valid) {
       const userData = this.userForm.value;
       if (this.mode === 'edit') {
+        console.log('test', this.userForm);
         this.userService.updateUser(userData);
       } else {
         this.userService.addUser(userData);
       }
+      this.userForm.patchValue(userData);
       this.router.navigate(['/users']);
     }
+  }
+
+  private setEducation(educationData: any[]): void {
+    const educationArray = this.userForm.get('education') as FormArray;
+    educationArray.clear();
+    educationData.forEach((edu) => {
+      educationArray.push(
+        this.fb.group({
+          institutionName: [edu.institutionName, Validators.required],
+          degree: [edu.degree, Validators.required],
+          date: [edu.date, Validators.required],
+        })
+      );
+    });
+  }
+
+  private setWorkExperience(workExperienceData: WorkExperience[]): void {
+    const workExpArray = this.userForm.get('workExperience') as FormArray;
+    workExpArray.clear();
+
+    if (workExperienceData) {
+      workExperienceData.forEach((work) => {
+        workExpArray.push(
+          this.fb.group({
+            companyName: [work.companyName, Validators.required],
+            position: [work.position, Validators.required],
+            date: [work.date, Validators.required],
+          })
+        );
+      });
+    }
+  }
+
+  addEducation() {
+    const educationForm = this.fb.group({
+      institutionName: ['', Validators.required],
+      degree: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+    this.education.push(educationForm);
+  }
+
+  removeEducation(index: number) {
+    this.education.removeAt(index);
+  }
+
+  addWorkExperience() {
+    const workExpForm = this.fb.group({
+      companyName: ['', Validators.required],
+      position: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+    this.workExperience.push(workExpForm);
+  }
+
+  removeWorkExperience(index: number) {
+    this.workExperience.removeAt(index);
   }
 
   cancel(): void {
