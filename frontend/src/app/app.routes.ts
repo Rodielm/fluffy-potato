c114e56fb435a61357280f2305178170d81ab145
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-20 12:53:41 +0100

    add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UserListComponent },
+  { path: 'users/new', component: UserFormComponent },
+  { path: 'users/:id/edit', component: UserFormComponent },
+  { path: 'users/:id/view', component: UserFormComponent },
+];
