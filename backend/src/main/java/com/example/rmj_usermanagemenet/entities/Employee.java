523e50542e98b29f3c5d35dc976eebfa7e61407e
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-20 11:38:03 +0100

    add annotation table and init script data

--- backend/src/main/java/com/example/rmj_usermanagemenet/entities/Employee.java
+++ backend/src/main/java/com/example/rmj_usermanagemenet/entities/Employee.java
@@ -2,26 +2,30 @@ package com.example.rmj_usermanagemenet.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable = false)
     private String name;
-    
-    @Column(name = "last_name")
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    
-    @ManyToOne
-    @JoinColumn(name = "department_id")
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
     private Department department;
 }
