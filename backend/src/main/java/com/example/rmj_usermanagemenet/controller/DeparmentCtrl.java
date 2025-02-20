76cd25c78f9ab90260cb8fb280ee3e18efa19263
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-20 11:14:56 +0100

    add department controller

---
+++ backend/src/main/java/com/example/rmj_usermanagemenet/controller/DeparmentCtrl.java
@@ -0,0 +1,24 @@
+package com.example.rmj_usermanagemenet.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rmj_usermanagemenet.dto.DepartmentDTO;
+import com.example.rmj_usermanagemenet.service.DepartmentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/departments")
+@RequiredArgsConstructor
+public class DeparmentCtrl {
+    private final DepartmentService departmentService;
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> createDepartment(@RequestBody DepartmentDTO department) {
+        return ResponseEntity.ok(departmentService.createDepartment(department));
+    }
+}
